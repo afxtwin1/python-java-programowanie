11eeaa56fdaccba57805d35c9c4cc1a009324254
Nikodem Dziwnik <nikodemdziwnik@gmail.com>
2025-02-27 21:50:36 +0100

    mala zmiana

--- src/java_files/BooleanLogic.java
+++ src/java_files/BooleanLogic.java
@@ -4,7 +4,7 @@ public class BooleanLogic {
     public static void main(String[] args) {
         System.out.println("Welcome to Boolean_Logic");
 
-        boolean result = 1==2;
+        boolean result = 2 == 1;
         System.out.println(result);
 
 
