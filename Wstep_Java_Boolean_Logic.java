88d08d7f5c61a13b99f4bdd61a8664e7ef70311d
Nikodem Dziwnik <nikodemdziwnik@gmail.com>
2025-02-27 19:00:22 +0100

    czyszczenie

--- Wstep_Java_Boolean_Logic.java
+++ Wstep_Java_Boolean_Logic.java
@@ -1,6 +1,12 @@
 public class Wstep_Java_Boolean_Logic {
     public static void main(String[] args) {
         System.out.println("Welcome to Boolean_Logic");
+
+        boolean result = false || false;
+        System.out.println(result);
+
+
+
     }
 }
 
--- out/production/Nauka-algorytmow-pythona/README.md
+++
@@ -1,4 +0,0 @@
-# Cześć!
- 
- ## Tutaj będę zamieszczał różne algorytmy i różne pliki w pythonie
- ### Jest to moje pierwsze prywatne repozytorium, na ktorym uczę się funkcji githuba i jak programować głównie w języku python
